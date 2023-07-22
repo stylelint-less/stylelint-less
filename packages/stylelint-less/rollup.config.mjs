9d442db49e14f806b336821f189af9633534100f
ubermanu <manu@paca.happy-dev.fr>
2023-07-22 16:30:52 +0200

    format rollup config

--- packages/stylelint-less/rollup.config.mjs
+++ packages/stylelint-less/rollup.config.mjs
@@ -2,17 +2,11 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-    input: 'src/index.js',
-    output: {
-        file: 'dist/index.js',
-        format: 'cjs',
-    },
-    external: [
-        /^postcss/,
-        /^stylelint/,
-    ],
-    plugins: [
-        resolve(),
-        commonjs(),
-    ]
-}
+	input: 'src/index.js',
+	output: {
+		file: 'dist/index.js',
+		format: 'cjs',
+	},
+	external: [/^postcss/, /^stylelint/],
+	plugins: [resolve(), commonjs()],
+};
